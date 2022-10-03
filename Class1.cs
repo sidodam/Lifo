523fc6c12f72508424a8d35a5764640fd6e6d033
sido <akou556@g.educaand.es>
2022-10-03 10:57:29 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		
+
+		public static void classy1()
+		{
+
+			global::System.Console.WriteLine("classy 1");
+		}
+	}
+}
---
+++ Lifo/Program.cs
@@ -0,0 +1,46 @@
+﻿
+
+
+
+var str = "(2+[3-12]*{8/3})";
+
+
+Console.WriteLine(Validator(str));
+
+
+static bool Validator(string input)
+{
+    if (string.IsNullOrEmpty(input))
+    {
+        return true;
+    }
+
+
+    Stack<char> brackets = new Stack<char>();
+
+    foreach (var c in input)
+    {
+        if (c == '[' || c == '{' || c == '(')
+            brackets.Push(c);
+        else if (c == ']' || c == '}' || c == ')')
+        {
+
+            if (brackets.Count == 0)
+                return false;
+
+            char open = brackets.Pop();
+
+            // Inconsistent brackets, e.g. (123]
+            if (c == '}' && open != '{' ||
+                c == ')' && open != '(' ||
+                c == ']' && open != '[')
+                return false;
+        }
+    }
+
+
+    if (brackets.Count > 0)
+        return false;
+
+    return true;
+}
